3807d7284a6e0d868b2e53d9d2fae6eb0e7a5764
dek <deeeqhaji@gmail.com>
2018-12-03 15:01:50 -0600

    added chickenMonkey

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,11 +1,12 @@
 
 for (let currentNumber = 0; currentNumber <= 100; currentNumber++) {
-     // Divide the current number by 5, and check if the remainder is 0
-    if (currentNumber % 5 === 0) {
-        console.log('Chicken')
-    } else if( currentNumber % 7 === 0) {
-        console.log('Monkey')
-    }else (
+    if(currentNumber % 5 === 0 && currentNumber % 7 === 0) {
+        console.log('chickenMonkey');
+    }else if (currentNumber % 7 === 0){
+        console.log('Monkey');
+    }else if (currentNumber % 5 === 0) {
+        console.log('chicken');
+    }else {
         console.log(currentNumber)
-    )
+    }
 }
